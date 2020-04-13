858829e21c04e5443ca1ce0866875c5525fd2786
chier <chier@chierdeMacBook-Pro.local>
2020-04-13 11:39:08 +0800

    mmap输出，更新同步方式

--- 初赛/code/main.cpp
+++ 初赛/code/main.cpp
@@ -121,6 +121,11 @@ class XJBG {
     int length;
   };
 
+ private:
+  std::string m_debug[8] = {
+      "",           "\t\t",         "\t\t\t",         "\t\t\t\t",
+      "\t\t\t\t\t", "\t\t\t\t\t\t", "\t\t\t\t\t\t\t", "\t\t\t\t\t\t\t\t"};
+
  public:
   void LoadData();           // 加载数据
   void TarJan();             // TarJan
@@ -129,31 +134,28 @@ class XJBG {
   void SaveAnswer(int pid);  // 保存答案
   void SortEdge();           // 儿子排序
   void WaitFork();           // 等待进程
+  void GetShmPtr();          // 获取共享数据句柄
 
  private:
   inline void addEdge(int u, int v, int w);  // 加边
   void doTarJan(int u);                      // tarjan
-  bool judge(const int &u, const int &dep);  // dfs剪枝
-  inline void SaveCircle(const int &dep);    // 保存环
   void BackSearch(int &st, int dep);         // 反向剪枝
   void doFindCircle(int u, int dep);         // 正向找环
   void createAnswerBuffers(int pid);         // 解析int
+  inline void connectBuffer(char *buffer, int &node, int &idx, char c);
 
  public:
   static const int MAXN = 200000 + 7;                   // 总点数
   static const int NTHREAD = 8;                         // 线程个数
   const int PARAM[NTHREAD] = {1, 2, 3, 4, 5, 6, 7, 8};  // 线程权重
-  // const int PARAM[NTHREAD] = {1, 2, 4, 20};  // 线程权重
-  std::string m_debug[8] = {
-      "",           "\t\t",         "\t\t\t",         "\t\t\t\t",
-      "\t\t\t\t\t", "\t\t\t\t\t\t", "\t\t\t\t\t\t\t", "\t\t\t\t\t\t\t\t"};
+  // const int PARAM[NTHREAD] = {2, 3, 5, 40};  // 线程权重
 
  private:
   int m_maxID = 0;                              // 最大点
-  std::vector<int> m_Circles;                   // 大于3的联通分量的点
   int m_Edges[MAXN][3][100];                    // u->[son, father, weight]
   int m_CountSons[MAXN], m_CountFathers[MAXN];  // 边数目
   int m_edgeNum = 0;                            // 边数目
+  std::vector<PreBuffer> m_MapID;               // 预处理答案
   int m_dfn[MAXN], m_low[MAXN];                 // Tarjan
   std::stack<int> m_stack;                      // Tarjan
   int m_category[MAXN];                         // 所在联通分量id
@@ -161,29 +163,42 @@ class XJBG {
   int m_tarjanCount = 0;                        // Tarjan搜索顺序
   int m_stackTop = 0;                           // Tarjan栈top标号
   int m_scc = 0, m_useScc = 0;                  // 总联通分量和大于3的
-  std::vector<PreBuffer> m_MapID;               // 预处理答案
+  std::vector<int> m_Circles;                   // 大于3的联通分量的点
 
  private:
   int m_answers = 0;                   // 环个数
   int m_start = 0;                     // 起点
-  int m_startctg = 0;                  // sccID
   std::vector<bool> m_OneReachable;    // 一步可达
   std::vector<bool> m_TwoReachable;    // 两步可达
   std::vector<bool> m_ThreeReachable;  // 三步可达
-  Vector<int> m_tempPath;              // dfs临时路径
   Vector<char *> m_Answer[5];          // 结果
   Vector<int> m_Count[5];              // 答案个数
-  std::vector<bool> m_vis;             // 标记
-  char *m_buffer[5];                   // 答案
-  uint32_t m_length[5];                // 答案长度
-  uint32_t m_totalLength = 0;          // 答案总长度
+  char *m_AnsBuf[5];                   // 答案
+  uint32_t m_AnsBufLen[5];             // bufferLength
 
  private:
   int m_TotalAnswers = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0600);
   int m_FlagFork = shmget(IPC_PRIVATE, NTHREAD * sizeof(int), IPC_CREAT | 0600);
+  int m_FlagSave = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0600);
+  uint32_t m_TotalBufferSize =
+      shmget(IPC_PRIVATE, sizeof(uint32_t), IPC_CREAT | 0600);
+  uint32_t m_BufferStart =
+      shmget(IPC_PRIVATE, sizeof(uint32_t), IPC_CREAT | 0600);
+
   int *m_TotalAnswersPtr;
   int *m_FlagForkPtr;
+  int *m_FlagSavePtr;
+  uint32_t *m_TotalBufferSizePtr;
+  uint32_t *m_BufferStartPtr;
 };
+
+void XJBG::GetShmPtr() {
+  m_TotalAnswersPtr = (int *)shmat(m_TotalAnswers, NULL, 0);
+  m_FlagForkPtr = (int *)shmat(m_FlagFork, NULL, 0);
+  m_FlagSavePtr = (int *)shmat(m_FlagSave, NULL, 0);
+  m_TotalBufferSizePtr = (uint32_t *)shmat(m_TotalBufferSize, NULL, 0);
+  m_BufferStartPtr = (uint32_t *)shmat(m_BufferStart, NULL, 0);
+}
 
 inline void XJBG::addEdge(int u, int v, int w) {
   m_Edges[u][0][m_CountSons[u]] = v;
@@ -278,88 +293,142 @@ void XJBG::TarJan() {
 #endif
 }
 
-void XJBG::BackSearch(int &u, int dep) {
-  for (int i = 0; i < m_CountFathers[u]; ++i) {
-    int v = m_Edges[u][1][i];
-    if (m_category[v] != m_startctg) {
-      m_vis[v] = true;
-      continue;
-    }
-    if (m_vis[v]) continue;
-    if (dep == 0) {
-      m_OneReachable[v] = true;
-      m_TwoReachable[v] = true;
-      m_ThreeReachable[v] = true;
-    } else if (dep == 1) {
-      m_TwoReachable[v] = true;
-      m_ThreeReachable[v] = true;
-    } else if (dep == 2) {
-      m_ThreeReachable[v] = true;
-      continue;
-    }
-    m_vis[v] = true;
-    this->BackSearch(v, dep + 1);
-    m_vis[v] = false;
-  }
-}
-
-// 0,1,2,3,4,5,6,0
-inline void XJBG::SaveCircle(const int &dep) {
-  int x = dep;
-  for (int i = 0; i < dep; ++i) {
-    x += m_MapID[m_tempPath[i]].length;
-  }
-  char *buf = new char[x];
-  int idx = 0;
-  for (int i = 0; i < dep; ++i) {
-    auto &mpid = m_MapID[m_tempPath[i]];
-    if (i != 0) buf[idx++] = ',';
-    memcpy(buf + idx, mpid.str + mpid.start, mpid.length);
-    idx += mpid.length;
-  }
-  buf[idx++] = '\n';
-  m_Answer[dep - 3].emplace_back(buf);
-  m_Count[dep - 3].emplace_back(idx);
-  ++m_answers;
-}
-
-// 1,2,3,4,5,6,7,1
-bool XJBG::judge(const int &v, const int &dep) {
-  if (m_vis[v]) return true;
-
-  switch (dep) {
-    case 4:
-      if (!m_ThreeReachable[v]) return true;
-      return false;
-      break;
-    case 5:
-      if (!m_TwoReachable[v]) return true;
-      return false;
-      break;
-    case 6:
-      if (m_OneReachable[v]) {
-        this->SaveCircle(dep + 1);
+void XJBG::BackSearch(int &st, int dep) {
+  for (int i = 0; i < m_CountFathers[st]; ++i) {
+    int v1 = m_Edges[st][1][i];
+    if (v1 <= st) continue;
+    m_OneReachable[v1] = true;
+    m_TwoReachable[v1] = true;
+    m_ThreeReachable[v1] = true;
+    for (int j = 0; j < m_CountFathers[v1]; ++j) {
+      int v2 = m_Edges[v1][1][j];
+      if (v2 <= st || v2 == v1) continue;
+      m_TwoReachable[v2] = true;
+      m_ThreeReachable[v2] = true;
+      for (int k = 0; k < m_CountFathers[v2]; ++k) {
+        int v3 = m_Edges[v2][1][k];
+        if (v3 <= st || v3 == v2 || v3 == v1) continue;
+        m_ThreeReachable[v3] = true;
       }
-      return true;
-      break;
-    default:
-      return false;
-      break;
+    }
   }
+}
+
+inline void XJBG::connectBuffer(char *buffer, int &node, int &idx, char c) {
+  memcpy(buffer + idx, m_MapID[node].str + m_MapID[node].start,
+         m_MapID[node].length);
+  idx += m_MapID[node].length;
+  buffer[idx++] = c;
 }
 void XJBG::doFindCircle(int u, int dep) {
-  for (int i = 0; i < m_CountSons[u]; ++i) {
-    int v = m_Edges[u][0][i];
-    m_tempPath[dep] = v;
-    if (dep > 2 && v == m_start) {
-      this->SaveCircle(dep);
-      continue;
-    } else if (this->judge(v, dep)) {
-      continue;
+  int sz0 = m_MapID[u].length;
+  for (int it1 = 0; it1 < m_CountSons[u]; ++it1) {
+    int v1 = m_Edges[u][0][it1];
+    if (v1 <= u) continue;
+    for (int it2 = 0; it2 < m_CountSons[v1]; ++it2) {
+      int v2 = m_Edges[v1][0][it2];
+      if (v2 <= u || v2 == v1) continue;
+      for (int it3 = 0; it3 < m_CountSons[v2]; ++it3) {
+        int v3 = m_Edges[v2][0][it3];
+        if (v3 == u) {
+          int sz = m_MapID[u].length + m_MapID[v1].length + m_MapID[v2].length;
+          char *buf = new char[sz + 3];
+          int idx = 0;
+          connectBuffer(buf, u, idx, ',');
+          connectBuffer(buf, v1, idx, ',');
+          connectBuffer(buf, v2, idx, '\n');
+          m_Answer[0].emplace_back(buf);
+          m_Count[0].emplace_back(idx);
+          ++m_answers;
+          continue;
+        }
+        if (v3 <= u || v3 == v2 || v3 == v1) continue;
+        for (int it4 = 0; it4 < m_CountSons[v3]; ++it4) {
+          int v4 = m_Edges[v3][0][it4];
+          if (v4 == u) {
+            int sz = m_MapID[u].length + m_MapID[v1].length +
+                     m_MapID[v2].length + m_MapID[v3].length;
+
+            char *buf = new char[sz + 4];
+            int idx = 0;
+            connectBuffer(buf, u, idx, ',');
+            connectBuffer(buf, v1, idx, ',');
+            connectBuffer(buf, v2, idx, ',');
+            connectBuffer(buf, v3, idx, '\n');
+            m_Answer[1].emplace_back(buf);
+            m_Count[1].emplace_back(idx);
+            ++m_answers;
+            continue;
+          }
+          if (v4 <= u || v4 == v3 || v4 == v2 || v4 == v1 ||
+              !m_ThreeReachable[v4])
+            continue;
+          for (int it5 = 0; it5 < m_CountSons[v4]; ++it5) {
+            int v5 = m_Edges[v4][0][it5];
+            if (v5 == u) {
+              int sz = m_MapID[u].length + m_MapID[v1].length +
+                       m_MapID[v2].length + m_MapID[v3].length +
+                       m_MapID[v4].length;
+              char *buf = new char[sz + 5];
+              int idx = 0;
+              connectBuffer(buf, u, idx, ',');
+              connectBuffer(buf, v1, idx, ',');
+              connectBuffer(buf, v2, idx, ',');
+              connectBuffer(buf, v3, idx, ',');
+              connectBuffer(buf, v4, idx, '\n');
+              m_Answer[2].emplace_back(buf);
+              m_Count[2].emplace_back(idx);
+              ++m_answers;
+              continue;
+            }
+            if (v5 <= u || v5 == v4 || v5 == v3 || v5 == v2 || v5 == v1 ||
+                !m_TwoReachable[v5])
+              continue;
+            for (int it6 = 0; it6 < m_CountSons[v5]; ++it6) {
+              int v6 = m_Edges[v5][0][it6];
+              if (v6 == u) {
+                int sz = m_MapID[u].length + m_MapID[v1].length +
+                         m_MapID[v2].length + m_MapID[v3].length +
+                         m_MapID[v4].length + m_MapID[v5].length;
+                char *buf = new char[sz + 6];
+                int idx = 0;
+                connectBuffer(buf, u, idx, ',');
+                connectBuffer(buf, v1, idx, ',');
+                connectBuffer(buf, v2, idx, ',');
+                connectBuffer(buf, v3, idx, ',');
+                connectBuffer(buf, v4, idx, ',');
+                connectBuffer(buf, v5, idx, '\n');
+                m_Answer[3].emplace_back(buf);
+                m_Count[3].emplace_back(idx);
+                ++m_answers;
+                continue;
+              }
+              if (v6 <= u || v6 == v5 || v6 == v4 || v6 == v3 || v6 == v2 ||
+                  v6 == v1)
+                continue;
+              if (m_OneReachable[v6]) {
+                int sz = m_MapID[u].length + m_MapID[v1].length +
+                         m_MapID[v2].length + m_MapID[v3].length +
+                         m_MapID[v4].length + m_MapID[v5].length +
+                         m_MapID[v6].length;
+                char *buf = new char[sz + 7];
+                int idx = 0;
+                connectBuffer(buf, u, idx, ',');
+                connectBuffer(buf, v1, idx, ',');
+                connectBuffer(buf, v2, idx, ',');
+                connectBuffer(buf, v3, idx, ',');
+                connectBuffer(buf, v4, idx, ',');
+                connectBuffer(buf, v5, idx, ',');
+                connectBuffer(buf, v6, idx, '\n');
+                m_Answer[4].emplace_back(buf);
+                m_Count[4].emplace_back(idx);
+                ++m_answers;
+              }
+            }
+          }
+        }
+      }
     }
-    m_vis[v] = true;
-    this->doFindCircle(v, dep + 1);
-    m_vis[v] = false;
   }
 }
 
@@ -385,37 +454,24 @@ void XJBG::FindCircle(int pid) {
 #ifdef TEST
   std::cerr << m_debug[pid] << pid << ": FindStart\n";
 #endif
-
-  m_TotalAnswersPtr = (int *)shmat(m_TotalAnswers, NULL, 0);
-  m_FlagForkPtr = (int *)shmat(m_FlagFork, NULL, 0);
-
   int sum = 0;
   for (int i = 0; i < NTHREAD; ++i) sum += PARAM[i];
   int block = m_Circles.size() / sum, st = 0;
   for (int i = 0; i < pid; ++i) st += PARAM[i] * block;
   int ed = (pid == NTHREAD - 1 ? m_Circles.size() : st + block * PARAM[pid]);
 
-  m_tempPath.init(7, -1);
-  m_vis = std::vector<bool>(m_maxID, false);
-  for (int i = 0; i < st; ++i) m_vis[m_Circles[i]] = true;
-
   for (int i = st; i < ed; ++i) {
-    m_OneReachable = std::vector<bool>(m_maxID, false);
-    m_TwoReachable = std::vector<bool>(m_maxID, false);
-    m_ThreeReachable = std::vector<bool>(m_maxID, false);
-
+    m_OneReachable = std::vector<bool>(m_Circles.size(), false);
+    m_TwoReachable = std::vector<bool>(m_Circles.size(), false);
+    m_ThreeReachable = std::vector<bool>(m_Circles.size(), false);
     int v = m_Circles[i];
     m_start = v;
-    m_startctg = m_category[v];
-    m_vis[v] = true;
     this->BackSearch(v, 0);
-    m_tempPath[0] = v;
     this->doFindCircle(v, 1);
   }
-  this->createAnswerBuffers(pid);
   *m_TotalAnswersPtr += m_answers;
-  m_FlagForkPtr[pid] = 1;
-  usleep(1);
+  this->createAnswerBuffers(pid);
+  m_FlagForkPtr[pid] = -1;
 #ifdef TEST
   std::cerr << m_debug[pid] << pid << ": FindEnd\n";
   std::cerr << m_debug[pid] << pid << ": " << m_answers << "\n";
@@ -426,38 +482,41 @@ void XJBG::FindCircle(int pid) {
 
 void XJBG::PreSave() {
   m_MapID.resize(m_maxID);
-  for (int i = 0; i < m_maxID; ++i) {
-    int x = i;
+  for (auto &v : m_Circles) {
+    int x = v;
     if (x == 0) {
-      m_MapID[i].start = 9;
-      m_MapID[i].str[9] = '0';
-      m_MapID[i].length = 1;
+      m_MapID[v].start = 9;
+      m_MapID[v].str[9] = '0';
+      m_MapID[v].length = 1;
       continue;
     }
     int idx = 10;
     while (x) {
-      m_MapID[i].str[--idx] = x % 10 + '0';
+      m_MapID[v].str[--idx] = x % 10 + '0';
       x /= 10;
     }
-    m_MapID[i].start = idx;
-    m_MapID[i].length = 10 - idx;
+    m_MapID[v].start = idx;
+    m_MapID[v].length = 10 - idx;
   }
 }
 void XJBG::createAnswerBuffers(int pid) {
   for (int len = 0; len < 5; ++len) {
     auto &ans = m_Answer[len];
     uint32_t tbufsize = (uint32_t)ans.n * (len + 3) * 11;
-    m_buffer[len] = new char[tbufsize];
+    m_AnsBuf[len] = new char[tbufsize];
     uint32_t tidx = 0;
     for (int k = 0; k < ans.n; ++k) {
-      memcpy(m_buffer[len] + tidx, ans[k], m_Count[len][k]);
+      memcpy(m_AnsBuf[len] + tidx, ans[k], m_Count[len][k]);
       tidx += m_Count[len][k];
     }
-    m_length[len] = tidx;
-    m_totalLength += tidx;
+    m_AnsBufLen[len] = tidx;
+    *m_TotalBufferSizePtr += tidx;
   }
 }
 void XJBG::SaveAnswer(int pid) {
+#ifdef TEST
+  std::cerr << m_debug[pid] << pid << ": SaveStart\n";
+#endif
   int answers = *m_TotalAnswersPtr;
   if (pid == 0) {
     int firIdx = 12;
@@ -471,47 +530,43 @@ void XJBG::SaveAnswer(int pid) {
         answers /= 10;
       }
     }
+
     int fd = open(RESULT, O_RDWR | O_CREAT, 0666);
-    char *result = (char *)mmap(NULL, 12 - firIdx, PROT_READ | PROT_WRITE,
-                                MAP_SHARED, fd, 0);
-    ftruncate(fd, 12 - firIdx);
-    memcpy(result, firBuf + firIdx, 12 - firIdx);
+    uint32_t bufsize = *m_TotalBufferSizePtr + (12 - firIdx);
+    char *result =
+        (char *)mmap(NULL, bufsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    ftruncate(fd, bufsize);
     close(fd);
+    memcpy(result, firBuf + firIdx, 12 - firIdx);
+    *m_BufferStartPtr = 12 - firIdx;
+    *m_FlagSavePtr = 0;
   }
-  m_FlagForkPtr = (int *)shmat(m_FlagFork, NULL, 0);
+
+  struct stat sb;
+  int fd = open(RESULT, O_RDWR | O_CREAT, 0666);
+  fstat(fd, &sb);
+  char *result =
+      (char *)mmap(NULL, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  close(fd);
+
   for (int len = 0; len < 5; ++len) {
-    if (pid == 0 && len == 0) {
-      m_buffer[len][m_length[len]] = 0;
-      FILE *fp = fopen(RESULT, "at");
-      fputs(m_buffer[len], fp);
-      fclose(fp);
-      m_FlagForkPtr[pid] = len;
-    }
-    if (pid == 0 && len != 0) {
-      while (m_FlagForkPtr[NTHREAD - 1] != len - 1) {
-        usleep(1);
-      }
-      m_buffer[len][m_length[len]] = 0;
-      FILE *fp = fopen(RESULT, "at");
-      fputs(m_buffer[len], fp);
-      fclose(fp);
-      m_FlagForkPtr[pid] = len;
-    }
-    if (pid != 0) {
-      while (m_FlagForkPtr[pid - 1] != len) {
-        usleep(1);
-      }
-      m_buffer[len][m_length[len]] = 0;
-      FILE *fp = fopen(RESULT, "at");
-      fputs(m_buffer[len], fp);
-      fclose(fp);
-      m_FlagForkPtr[pid] = len;
+    while (*m_FlagSavePtr != pid * 5 + len) usleep(1);
+    memcpy(result + *m_BufferStartPtr, m_AnsBuf[len], m_AnsBufLen[len]);
+    *m_BufferStartPtr += m_AnsBufLen[len];
+    if (pid == NTHREAD - 1) {
+      *m_FlagSavePtr = len + 1;
+    } else {
+      *m_FlagSavePtr = (pid + 1) * 5 + len;
     }
   }
 
+#ifdef TEST
+  std::cerr << m_debug[pid] << pid << ": SaveEnd\n";
+#endif
 #ifdef LOCAL
   if (pid == 0) {
     std::cerr << "TotalAnswer: " << *m_TotalAnswersPtr << "\n";
+    std::cerr << "TotalBufferSize: " << *m_TotalBufferSizePtr << "\n";
   }
 #endif
 }
@@ -520,7 +575,7 @@ void XJBG::WaitFork() {
   while (true) {
     int x = 0;
     for (int i = 0; i < NTHREAD; ++i) x += m_FlagForkPtr[i];
-    if (x == NTHREAD) {
+    if (x == -NTHREAD) {
       return;
     }
     usleep(1);
@@ -532,8 +587,8 @@ int main() {
 
   XJBG *xjbg = new XJBG();
   xjbg->LoadData();
-  xjbg->PreSave();
   xjbg->TarJan();
+  xjbg->PreSave();
   xjbg->SortEdge();
 
   pid_t Children[XJBG::NTHREAD - 1] = {0};
@@ -549,10 +604,15 @@ int main() {
     }
   }
 
+  xjbg->GetShmPtr();
   xjbg->FindCircle(pid);
   xjbg->WaitFork();
   xjbg->SaveAnswer(pid);
 
   if (pid != 0) exit(0);
+
+  // for (int i = 0; i < XJBG::NTHREAD - 1; ++i) {
+  //   waitpid(Children[i], NULL, 0);
+  // }
   return 0;
 }
